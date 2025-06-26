7179429404eeab2168b3ae7efc77bd55f75cfb70
Amine <amine.benlala@gamil.com>
2025-06-26 17:41:10 +0100

    watch store andt mouse store

---
+++ Mouse_Store/assets/js/main.js
@@ -0,0 +1,14 @@
+// as before swiper
+const swiper = new Swiper(".gallery-swiper", {
+    sildesPerView: 1,
+    spacebetween:20,
+    pagination: {
+        el: ".swiper-pagination",
+        clicable: true
+    }
+}
+);
+
+// scroll
+
+// nav
